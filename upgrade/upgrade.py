9f2fd6944da76fdad7cc50a11eb5e9cf6f114b64
Artur Svechnikov <asvechnikov@mirantis.com>
2015-08-07 13:36:51 +0300

    Create base

---
+++ upgrade/upgrade.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sys
+
+from cliff import app
+from cliff import commandmanager as cm
+
+class UpgradeApp(app.App):
+
+    def __init__(self, **kwargs):
+        super(UpgradeApp, self).__init__(
+            description='Super upgrade script',
+            command_manager=cm.CommandManager('upgrade'),
+            **kwargs)
+
+    def initialize_app(self, argv):
+        self.LOG.debug('initialize_app')
+
+    def prepare_to_run_command(self, cmd):
+        self.LOG.debug('prepare to run command %s', cmd.__class__.__name__)
+
+    def clean_up(self, cmd, result, err):
+        self.LOG.debug('clean up %s', cmd.__class__.__name__)
+        if err:
+            self.LOG.debug('got an error: %s', err)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    return UpgradeApp().run(argv)
